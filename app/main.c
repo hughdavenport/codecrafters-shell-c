ffe3245dfde2bc5f466c2aa2db785a0a37514857
Hugh Davenport <hugh@davenport.net.nz>
2025-01-10 12:54:41 +1300

    Show prompt

--- app/main.c
+++ app/main.c
@@ -4,8 +4,7 @@ int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  // printf("$ ");
+  printf("$ ");
 
   // Wait for user input
   char input[100];
