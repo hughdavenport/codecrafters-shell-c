05f3caf277b5af4c53f78e9d345875c78ccf9712
Hugh Davenport <hugh@davenport.net.nz>
2025-01-10 13:03:20 +1300

    Parse command from line

--- app/main.c
+++ app/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   // Flush after every printf
@@ -9,5 +10,10 @@ int main() {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+
+  char *command = strtok(input, " \n");
+
+  fprintf(stderr, "%s: command not found\n", command);
+
   return 0;
 }
