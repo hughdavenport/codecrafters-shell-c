2229b1995c0dfccf92b20561a1af95dc00f0a51e
Hugh Davenport <hugh@davenport.net.nz>
2025-01-13 17:27:19 +1300

    Add cd builtin

--- app/main.c
+++ app/main.c
@@ -1,10 +1,13 @@
 #include <assert.h>
+#include <errno.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#include <pwd.h>
 
 #include <sys/wait.h>
 
@@ -62,6 +65,52 @@ char *read_arg(char *string, const char *delim, char **rest) {
   }
 }
 
+extern char **environ;
+int run_program(char *file_path, char *command, char *rest, char *delim) {
+  ARRAY(char *) argv = {0};
+  ARRAY_ADD(argv, command);
+  char *arg;
+  while ((arg = read_arg(rest, delim, &rest)) != NULL) {
+    ARRAY_ADD(argv, arg);
+  }
+
+  ARRAY_ADD(argv, NULL);
+  pid_t pid = fork();
+  switch (pid) {
+    case -1:
+      assert(false && "Out of memory");
+      UNREACHABLE();
+      return -1;
+
+    case 0:
+      assert(execve(file_path, argv.data, environ) != -1);
+      UNREACHABLE();
+      return -1;
+
+    default: {
+      int wstatus = 0;
+      assert(waitpid(pid, &wstatus, 0) != -1);
+      for (size_t i = 1; i < argv.size; i ++) {
+        free(argv.data[i]);
+      }
+      free(argv.data);
+      return WEXITSTATUS(wstatus);
+    }
+  }
+  UNREACHABLE();
+  return -1;
+}
+
+
+int help_command(char *command, char *rest, char *delim) {
+    printf("Available commands:\n");
+    for (size_t i = 0; i < builtins.size; i ++) {
+      command_t cmd = builtins.data[i];
+      printf("    %-10s - %s\n", cmd.command, cmd.description);
+    }
+    return 0;
+}
+
 int exit_command(char *command, char *rest, char *delim) {
   char *arg = read_arg(rest, delim, &rest);
 
@@ -97,15 +146,6 @@ int echo_command(char *command, char *rest, char *delim) {
   }
   printf("\n");
   return 0;
-}
-
-int help_command(char *command, char *rest, char *delim) {
-    printf("Available commands:\n");
-    for (size_t i = 0; i < builtins.size; i ++) {
-      command_t cmd = builtins.data[i];
-      printf("    %-10s - %s\n", cmd.command, cmd.description);
-    }
-    return 0;
 }
 
 int type_command(char *command, char *rest, char *delim) {
@@ -171,48 +211,132 @@ int pwd_command(char *command, char *rest, char *delim) {
   return 0;
 }
 
-extern char **environ;
-int run_program(char *file_path, char *command, char *rest, char *delim) {
-  ARRAY(char *) argv = {0};
-  ARRAY_ADD(argv, command);
-  char *arg;
-  while ((arg = read_arg(rest, delim, &rest)) != NULL) {
-    ARRAY_ADD(argv, arg);
+int cd(char *file_path) {
+  int ret = chdir(file_path);
+  if (ret < 0) {
+    switch (errno) {
+      case EACCES:
+        fprintf(stderr, "cd: %s: Permission denied\n", file_path);
+        return 1;
+
+      case ENOENT:
+      case ENOTDIR:
+        fprintf(stderr, "cd: %s: No such file or directory\n", file_path);
+        return 1;
+    }
+    assert(false && "Uncaught error occured in chdir");
+    UNREACHABLE();
+    return 1;
+  }
+  return 0;
+}
+
+int cd_command(char *command, char *rest, char *delim) {
+  char *arg = read_arg(rest, delim, &rest);
+  if (arg == NULL) {
+    char *home = getenv("HOME");
+    if (home == NULL) {
+      printf("cd: HOME not set\n");
+      return 1;
+    }
+    return cd(home);
   }
 
-  ARRAY_ADD(argv, NULL);
-  pid_t pid = fork();
-  switch (pid) {
-    case -1:
-      assert(false && "Out of memory");
-      UNREACHABLE();
-      return -1;
+  char *next = read_arg(rest, delim, &rest);
+  if (next != NULL) {
+    printf("cd: too many arguments\n");
+    return 1;
+  }
 
-    case 0:
-      assert(execve(file_path, argv.data, environ) != -1);
-      UNREACHABLE();
-      return -1;
+  int ret = 0;
+  if (arg[0] == '~') {
+    struct passwd *passwd = NULL;
+    switch (arg[1]) {
+      case '\0': {
+        free(arg);
+        uid_t me = getuid();
+        while ((passwd = getpwent()) != NULL) {
+          if (passwd->pw_uid == me) {
+            setpwent();
+            endpwent();
+            return cd(passwd->pw_dir);
+          }
+        }
+        setpwent();
+        endpwent();
+        UNREACHABLE();
+      }; break;
 
-    default: {
-      int wstatus = 0;
-      assert(waitpid(pid, &wstatus, 0) != -1);
-      for (size_t i = 1; i < argv.size; i ++) {
-        free(argv.data[i]);
-      }
-      free(argv.data);
-      return WEXITSTATUS(wstatus);
+      case '/': {
+        uid_t me = getuid();
+        while ((passwd = getpwent()) != NULL) {
+          if (passwd->pw_uid == me) {
+            setpwent();
+            endpwent();
+            size_t len = strlen(passwd->pw_dir);
+            size_t arg_len = strlen(arg + 1);
+            char *full_path = malloc(len + arg_len + 1);
+            assert(full_path != NULL);
+            strcpy(full_path, passwd->pw_dir);
+            strcpy(full_path + len, arg + 1);
+            full_path[len + arg_len] = '\0';
+            free(arg);
+            int ret = cd(full_path);
+            free(full_path);
+            return ret;
+          }
+        }
+        setpwent();
+        endpwent();
+        UNREACHABLE();
+      }; break;
+
+      default: {
+        // ~user
+        while ((passwd = getpwent()) != NULL) {
+          size_t len = strlen(passwd->pw_name);
+          if (strncmp(arg + 1, passwd->pw_name, len) == 0) {
+            setpwent();
+            endpwent();
+            switch (arg[len + 1]) {
+              case '\0':
+                free(arg);
+                return cd(passwd->pw_dir);
+
+              case '/': {
+                size_t dir_len = strlen(passwd->pw_dir);
+                size_t arg_len = strlen(arg + len + 1);
+                char *full_path = malloc(dir_len + arg_len + 1);
+                assert(full_path != NULL);
+                strcpy(full_path, passwd->pw_dir);
+                strcpy(full_path + dir_len, arg + len + 1);
+                full_path[len + arg_len] = '\0';
+                free(arg);
+                ret = cd(full_path);
+                free(full_path);
+                return ret;
+              }
+            }
+          }
+        }
+        setpwent();
+        endpwent();
+        // Haven't found user, fall out to normal cd
+      }; break;
     }
   }
-  UNREACHABLE();
-  return -1;
+  ret = cd(arg);
+  free(arg);
+  return ret;
 }
 
 int main(int argc, char **argv) {
+  ARRAY_ADD(builtins, COMMAND(help, "Displays help about commands."));
   ARRAY_ADD(builtins, COMMAND(exit, "Exit the shell, with optional code."));
   ARRAY_ADD(builtins, COMMAND(echo, "Prints any arguments to stdout."));
   ARRAY_ADD(builtins, COMMAND(type, "Prints the type of command arguments."));
   ARRAY_ADD(builtins, COMMAND(pwd, "Prints current working directory."));
-  ARRAY_ADD(builtins, COMMAND(help, "Displays help about commands."));
+  ARRAY_ADD(builtins, COMMAND(cd, "Change current working directory."));
 
   // Flush after every printf
   setbuf(stdout, NULL);
